32e2a0e0b9995b2f6048217538aaa6d3c374df2b
Joshua Canton <jtcanton@uw.edu>
2018-11-29 15:15:27 -0800

    made function for filtered data range file

--- Filtered_data_range.R
+++ Filtered_data_range.R
@@ -2,17 +2,27 @@ library(magrittr)
 library(dplyr)
 library(lubridate)
 
-#change filepath to fit your computer
-crime_data <- read.csv(file.path("C:", "Users", "dark_","Desktop", "info201", "info201_TeamRepo", "Crime_Data.csv"))
 
+#put your file path to the Crime_Data.csv into this function to filter it to the appropriate years of interest for this project
+filter_dates <- function(file_path){
+
+crime_data <- read.csv(file_path)  
+  
 #uses lubridate to construct a Year Occurred column
 data <- 
   crime_data %>% 
   mutate(Date.Occurred = mdy(Occurred.Date)) %>% 
-  select(-Occurred.Time, -Occurred.Date) %>% 
+  select(-Occurred.Date) %>% 
   mutate(Year.Occurred = year(Date.Occurred))
 
 #filters the data to just 2008-2018
 filtered_data <- 
   data %>% 
   filter(Year.Occurred == '2008' | Year.Occurred == '2009' | Year.Occurred == '2010' | Year.Occurred == '2011' | Year.Occurred == '2012' | Year.Occurred == '2013' | Year.Occurred == '2014' | Year.Occurred == '2015' | Year.Occurred == '2016' | Year.Occurred == '2017' | Year.Occurred == '2018')
+}
+
+#SOURCE THIS FILE AND CALL THE filter_dates() FUNCTION IN YOUR FILE LIKE THIS EXAMPLE:
+# crime_data_08_thru_18 <- filter_dates("YOUR FILE PATH")
+
+
+
